58fa581ae91a125d5027a25ff6874aa7af650649
Your Name <you@example.com>
2023-09-09 21:26:28 +0500

    Валидация для публикации постов(без БД)

--- post/user_post_api.py
+++ post/user_post_api.py
@@ -1,18 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, Body
+
+from post import PublicPostModel, EditPostModel
 
 # Создать компонент
 posts_router = APIRouter(prefix='/user_post', tags=['Работа с публикациями'])
 
 @posts_router.post('/public_post')
-async def public_post():
+async def public_post(data: PublicPostModel):
     pass
 
 @posts_router.put('/change_post')
-async def change_post():
+async def change_post(data: EditPostModel):
     pass
 
 @posts_router.delete('/delete_post')
-async def delete_post():
+async def delete_post(post_id: int, user_id: int):
     pass
 
 @posts_router.get('/get_all_posts')
@@ -20,11 +22,13 @@ async def get_all_posts():
     pass
 
 @posts_router.post('/add_photo')
-async def add_photo():
+async def add_photo(post_id: int = Body(),
+                    user_id: int = Body(),
+                    photo_file: UploadFile = None):
     pass
 
 @posts_router.delete('/delete_photo')
-async def delete_photo():
+async def delete_photo(photo_id: int, user_id: int):
     pass
 
 
